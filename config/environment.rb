77ed40dea89681ec2999feace2d96f62ecb5ab7e
peter-keller <keller.f.peter@gmail.com>
2019-03-25 20:12:07 +0000

    init app, mysql connect, models ready

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-require 'bundler'
-Bundler.require
+# Load the Rails application.
+require_relative 'application'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+# Initialize the Rails application.
+Rails.application.initialize!
